5f1634559b26e8cdd92b857bf999e67b2fb6a5c1
William Ma <willzma15@gmail.com>
2016-01-23 15:14:10 -0500

    Full screen button that saves numbered jpeg's

--- app/src/main/java/willzma/iris/MainActivity.java
+++ app/src/main/java/willzma/iris/MainActivity.java
@@ -1,21 +1,63 @@
 package willzma.iris;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    int TAKE_PHOTO_CODE = 0;
+    protected int count = 0;
+    protected String dir;
+    protected File newDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                + "/picFolder/";
+        newDir = new File(dir);
+        newDir.mkdirs();
+
+        Button takePicture = (Button) findViewById(R.id.takePicture);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                count++;
+                String file = dir + count + ".jpg";
+                File newFile = new File(file);
+
+                try {
+                    newFile.createNewFile();
+                } catch (IOException e) {
+
+                }
+
+                Uri outputFileUri = Uri.fromFile(newFile);
+
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+            }
+        });
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -25,7 +67,16 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+            Log.d("Iris", "Picture saved");
+        }
     }
 
     @Override
